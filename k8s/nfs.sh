134c42cc50e88fbce43eef6b658e75651002e226
Divye Kapoor <divyekapoor@gmail.com>
2022-06-19 02:00:19 -0700

    use a subdir for nfs storage for K8s

--- k8s/nfs.sh
+++ k8s/nfs.sh
@@ -6,7 +6,7 @@ echo "Installing NFS subdir external provisioner."
 helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
 helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
     --set nfs.server=192.168.50.142 \
-    --set nfs.path=/volumeUSB1/usbshare \
+    --set nfs.path=/volumeUSB1/usbshare/pvc \
     --namespace nfs-subdir-external-provisioner \
     --create-namespace
 
