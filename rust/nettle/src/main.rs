cdc88c9abdbf448b913d16c0033470c3243a4a8a
Divye Kapoor <divyekapoor@gmail.com>
2022-09-03 00:09:54 -0700

    Nettle data.

--- rust/nettle/src/main.rs
+++ rust/nettle/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let config_str = include_str!("../data/log4rs.yaml");
+    let config = serde_yaml::from_str(config_str).expect(
+        "expected config_str to be available");
+    log4rs::init_raw_config(config).expect(
+        "log4rs initiatlization failed!");
+
+    log::info!("Log4rs config: \n {:}", config_str);
 }
