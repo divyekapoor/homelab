a96cf6183df614d7532149ccee2f683ca3ccdc2f
Divye Kapoor <divyekapoor@gmail.com>
2022-09-08 20:10:01 -0700

    More rust implementations.

--- rust/nettle_py/src/lib.rs
+++ rust/nettle_py/src/lib.rs
@@ -1,7 +1,75 @@
+use eyre::Result;
 use log::info;
 use log4rs::config::RawConfig;
 use pyo3::prelude::*;
-use eyre::{Result};
+// use std::ops::Drop;
+// use derivative::Derivative;
+use std::collections::HashMap;
+use std::sync::Mutex;
+// use std::default::Default;
+// use once_cell::sync::OnceCell;
+// use dashmap::DashMap;
+use pyo3::exceptions::PyRuntimeError;
+use zmq2::SocketType;
+
+#[pyclass]
+#[derive(Debug, Eq, PartialEq, Hash)]
+struct NettleHandle {
+    #[pyo3(get)]
+    port: i32,
+}
+
+struct NettleOperator {
+    zmq_socket: zmq2::Socket,
+}
+
+struct NettleGlobals {
+    zmq_context: zmq2::Context,
+
+    nettle_map: HashMap<NettleHandle, NettleOperator>,
+}
+
+impl Default for NettleGlobals {
+    fn default() -> Self {
+        NettleGlobals {
+            zmq_context: zmq2::Context::new(),
+            nettle_map: HashMap::new(),
+        }
+    }
+}
+
+#[pymethods]
+impl NettleHandle {
+    #[new]
+    fn new(port: i32) -> Result<Self> {
+        let handle = NettleHandle { port: port };
+        let nettle_globals = globals::get::<NettleGlobals>();
+        nettle_globals.nettle_map.insert(
+            handle.clone(),
+            NettleOperator {
+                zmq_socket: nettle_globals
+                    .zmq_context
+                    .socket(SocketType::REQ)
+                    .map_err(|_| PyRuntimeError::new_err("Unable to create socket"))?,
+            },
+        );
+        Ok(handle)
+    }
+
+    fn log(&self) {
+        info!("Object is: {:?}", self);
+    }
+
+    fn listen(&self) -> Result<Self> {
+        let nettle_globals = globals::get::<NettleGlobals>();
+        let nettle_operator = nettle_globals
+            .nettle_map
+            .get(self)
+            .ok_or_else(|| PyRuntimeError::new_err(""))?
+            .value_mut();
+        Ok(*self)
+    }
+}
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
@@ -10,7 +78,6 @@ fn sum_as_string(a: usize, b: usize) -> Result<String> {
     Ok((a + b).to_string())
 }
 
-#[pyfunction]
 fn init_logging() -> Result<()> {
     let config_str = include_str!("../data/log4rs.yaml");
     let config: RawConfig = serde_yaml::from_str(config_str)?;
@@ -18,20 +85,12 @@ fn init_logging() -> Result<()> {
     Ok(())
 }
 
-#[pyfunction]
-fn init_logging_with(config_str: &str) -> Result<()> {
-    let config : RawConfig = serde_yaml::from_str(config_str)?;
-    log4rs::init_raw_config(config)?;
-
-    Ok(())
-}
-
 /// A Python module implemented in Rust.
 #[pymodule]
 fn nettle_py(_py: Python, m: &PyModule) -> PyResult<()> {
     color_eyre::install()?;
+    init_logging()?;
+    m.add_class::<NettleHandle>()?;
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-    m.add_function(wrap_pyfunction!(init_logging, m)?)?;
-    m.add_function(wrap_pyfunction!(init_logging_with, m)?)?;
     Ok(())
 }
