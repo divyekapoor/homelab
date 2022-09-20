f07254a8c42fbdfe37c7b8a5669494980c510c3e
Divye Kapoor <divyekapoor@gmail.com>
2022-09-20 00:35:19 -0700

    Restructure the code to manage the socket with the python object

--- rust/nettle_py/src/lib.rs
+++ rust/nettle_py/src/lib.rs
@@ -4,55 +4,36 @@ use log4rs::config::RawConfig;
 use pyo3::prelude::*;
 // use std::ops::Drop;
 // use derivative::Derivative;
-use std::collections::HashMap;
-use std::sync::Mutex;
+// use std::collections::HashMap;
+// use std::sync::Mutex;
 // use std::default::Default;
 // use once_cell::sync::OnceCell;
 // use dashmap::DashMap;
-use pyo3::exceptions::PyRuntimeError;
-use zmq2::SocketType;
+// use pyo3::exceptions::PyRuntimeError;
+use zmq::SocketType;
+use derivative::Derivative;
 
 #[pyclass]
-#[derive(Debug, Eq, PartialEq, Hash)]
+#[derive(Derivative)]
+#[derivative(Debug, Eq, PartialEq, Hash)]
 struct NettleHandle {
     #[pyo3(get)]
     port: i32,
-}
 
-struct NettleOperator {
-    zmq_socket: zmq2::Socket,
-}
-
-struct NettleGlobals {
-    zmq_context: zmq2::Context,
-
-    nettle_map: HashMap<NettleHandle, NettleOperator>,
-}
-
-impl Default for NettleGlobals {
-    fn default() -> Self {
-        NettleGlobals {
-            zmq_context: zmq2::Context::new(),
-            nettle_map: HashMap::new(),
-        }
-    }
+    #[derivative(PartialEq="ignore")]
+    #[derivative(Hash="ignore")]
+    #[derivative(Debug="ignore")]
+    zmq_socket: zmq::Socket
 }
 
 #[pymethods]
 impl NettleHandle {
     #[new]
     fn new(port: i32) -> Result<Self> {
-        let handle = NettleHandle { port: port };
-        let nettle_globals = globals::get::<NettleGlobals>();
-        nettle_globals.nettle_map.insert(
-            handle.clone(),
-            NettleOperator {
-                zmq_socket: nettle_globals
-                    .zmq_context
-                    .socket(SocketType::REQ)
-                    .map_err(|_| PyRuntimeError::new_err("Unable to create socket"))?,
-            },
-        );
+        let zmq_context = zmq::Context::new();
+        let zmq_socket = zmq_context.socket(SocketType::REQ)?;
+
+        let handle = NettleHandle { port: port, zmq_socket: zmq_socket };
         Ok(handle)
     }
 
@@ -60,22 +41,12 @@ impl NettleHandle {
         info!("Object is: {:?}", self);
     }
 
-    fn listen(&self) -> Result<Self> {
-        let nettle_globals = globals::get::<NettleGlobals>();
-        let nettle_operator = nettle_globals
-            .nettle_map
-            .get(self)
-            .ok_or_else(|| PyRuntimeError::new_err(""))?
-            .value_mut();
-        Ok(*self)
+    fn listen(&self) -> Result<()> {
+        self.zmq_socket.bind(
+            &("tcp://0.0.0.0:".to_owned() + &self.port.to_string()))?;
+        info!("Listening to port: {:?}", self.port);
+        Ok(())
     }
-}
-
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> Result<String> {
-    info!("Sum as string: ({a}, {b})");
-    Ok((a + b).to_string())
 }
 
 fn init_logging() -> Result<()> {
@@ -91,6 +62,5 @@ fn nettle_py(_py: Python, m: &PyModule) -> PyResult<()> {
     color_eyre::install()?;
     init_logging()?;
     m.add_class::<NettleHandle>()?;
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
 }
